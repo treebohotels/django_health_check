54de5be4a08a7ce2fa5231f1d0e10f31dd30596b
Shashank Rao <shashank.rao@treebohotels.com>
2017-12-13 14:47:24 +0530

    Instead of saving and deleting objects, check db connection

--- health_check/db/backends.py
+++ health_check/db/backends.py
@@ -1,22 +1,16 @@
-from django.db import DatabaseError, IntegrityError
+from django.db import connections
+from django.db.utils import OperationalError
 
 from health_check.backends import BaseHealthCheckBackend
-from health_check.exceptions import (
-    ServiceReturnedUnexpectedResult, ServiceUnavailable
-)
-
-from .models import TestModel
+from health_check.exceptions import ServiceUnavailable
 
 
 class DatabaseBackend(BaseHealthCheckBackend):
 
     def check_status(self):
+
         try:
-            obj = TestModel.objects.create(title="test")
-            obj.title = "newtest"
-            obj.save()
-            obj.delete()
-        except IntegrityError:
-            raise ServiceReturnedUnexpectedResult("Integrity Error")
-        except DatabaseError:
-            raise ServiceUnavailable("Database error")
+            for db_conn in connections:
+                db_conn.cursor()
+        except OperationalError:
+            ServiceUnavailable("Database error")
