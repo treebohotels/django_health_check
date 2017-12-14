a8f4807f96f3a725010e14873dd4137ad51ab033
shashank.rao <shashank.rao@treebohotels.com>
2017-12-14 17:36:04 +0530

    Removed migrations

--- health_check/db/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-
-class TestModel(models.Model):
-    title = models.CharField(max_length=128)
-
-    class Meta:
-        db_table = 'health_check_db_testmodel'
